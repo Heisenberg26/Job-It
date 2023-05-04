f32cfaba6f4d504e1665e6bee1fe4737731369dc
ashu260 <devuashutosh26@gmail.com>
2023-05-04 14:48:17 +0530

    App Working through Expo Server

--- Job-It/app/index.js
+++ Job-It/app/index.js
@@ -1,11 +1,14 @@
 import { useState } from 'react';
-import { view, Text }  from 'react-native'
+import { View, Text }  from 'react-native'
+import { color } from 'react-native-reanimated';
 
 const Home = () => {
     return (
-        <view>
-            <Text>Home</Text>
-        </view>
+        <View>
+            <Text style={{color:'blue',fontWeight: 'bold',
+      fontSize: 100, marginTop: 20,
+      textAlign: 'center',}}>SIDDHARTH IS A BITCH </Text>
+        </View>
     )
 }
 
